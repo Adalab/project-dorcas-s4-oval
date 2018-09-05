031b94dac8190dea52af941634a8c189dcd948b8
Lau <lau.mc84@gmail.com>
2018-09-05 19:56:52 +0200

    basic distribution satisfaction

--- src/components/Satisfaction.js
+++ src/components/Satisfaction.js
@@ -3,14 +3,32 @@ import '../styles/Satisfaction.css';
 
 class Satisfaction extends React.Component {
 
-	render () {
+  render() {
 
-		return (
+    return (
+      <section className="section__satisfaction">
+        <div class="row h-100">
+          <div class="col-2 h-100 satisfation__menu">
+            <div className="container_date-input">
+              <div className="container_date">
+                <label for="day_init">Fecha de inicio</label>
+                <input type="date" />
+              </div>
+              <div className="container_date">
+                <label for="party">Fecha de fin</label>
+                <input type="date" />
+              </div>
+            </div>
+          </div>
+          <div class="col-10 h-100 satisfation__info">
+            <div className="satisfation__media-equip"></div>
+            <div className="satisfation__media-personal"></div>
 
-			<section className="section__satisfaction"></section>
-
-		);
-	}
+          </div>
+        </div>
+      </section>
+    );
+  }
 
 }
 
