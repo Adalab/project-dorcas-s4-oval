7a80220de2c6dde8922dcdb131ea78e9d0af33f9
Aylecilla <aylenortiz@gmail.com>
2018-09-06 11:07:50 +0200

    add satisfaction dummy charts

--- src/components/ChartsSatisfaction.js
+++ src/components/ChartsSatisfaction.js
@@ -1,22 +1,12 @@
 import React from "react";
 import "../styles/ChartsSatisfaction.css";
+import barsUser from "../images/bars-users.jpg";
 
 class ChartsSatisfaction extends React.Component {
   render() {
     return (
       <div className="satisfation__media-personal">
-        <div className="satisfaction__media-personal--data media-personal1">
-          user 1
-        </div>
-        <div className="satisfaction__media-personal--data media-personal2">
-          user 2
-        </div>
-        <div className="satisfaction__media-personal--data media-personal3">
-          user 3
-        </div>
-        <div className="satisfaction__media-personal--data media-personal4">
-          user 4
-        </div>
+        <img src={barsUser} alt="" className="photo__media" />
       </div>
     );
   }
