b40ba9f2de57200009fa830ac50dac5c6cb16d73
Anna Branco <39792862+annabranco@users.noreply.github.com>
2018-09-06 09:10:13 +0200

    Update Satisfaction.js

--- src/components/Satisfaction.js
+++ src/components/Satisfaction.js
@@ -9,12 +9,12 @@ class Satisfaction extends React.Component {
       <section className="section__satisfaction">
         <div class="row h-100">
           <div class="col-2 h-100 satisfation__menu">
-            <div className="container_date-input">
-              <div className="container_date">
+            <div className="container__date-input">
+              <div className="container__date">
                 <label for="day_init">Fecha de inicio</label>
                 <input type="date" />
               </div>
-              <div className="container_date">
+              <div className="container__date">
                 <label for="party">Fecha de fin</label>
                 <input type="date" />
               </div>
