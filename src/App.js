eddb0b8f9bd069c089f63d3550a262bf4a7582cf
Anna <annya.branco@gmail.com>
2018-09-11 09:23:08 +0200

    keys.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react';
 import './styles/App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import Keys from './keys/keys';
+
+const urlTrello = `https://api.trello.com/1/boards/n70jUITJ/lists?cards=all&card_fields=id%2Cname%2CidMembers%2Clabels&filter=open&fields=id%2Cname&key=${key}&token=${token}`;
 
 class App extends Component {
+constructor(props) {
+	super(props)
+
+	this.state = {
+		dataPlanning: [],
+		dataSatisfaction: []
+	}
+}
+
+
+componentDidMount() {
+
+}
+
+
   render() {
     return (
       <div className="App">
