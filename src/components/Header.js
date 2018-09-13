8dc89e1f9ec4caa6462e85350af12ecc22d363d6
lauradml <laura.domingo.82@gmail.com>
2018-09-13 18:12:44 +0200

    delete logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "../styles/Header.css";
-import logo from "../images/logo-nfq.png";
 import { Link } from "react-router-dom";
 class Header extends React.Component {
   render() {
     return (
       <header className="header">
         <div className="header__container-name">
-          <div className="header__container-logo">
-            <img src={logo} alt="" className="header__logo" />
-          </div>
           <h1 className="titleApp">OVAL</h1>
         </div>
         <nav className="header__container-nav">
