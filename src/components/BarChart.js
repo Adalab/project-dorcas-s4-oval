7e021291bb5b30618399f474cb9bf77fcf0e6a54
Lau <lau.mc84@gmail.com>
2018-09-17 19:08:50 +0200

    map for valuefield

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -6,187 +6,18 @@ import Chart, {
 import * as _ from 'lodash';
 
 
-const dataLaberList = [
-	{
-		list: "Backlog",
-		nourgente: 5,
-		fueradesprint: 4,
-		feature: 5,
-		incidenciadeseguridadolegal:5,
-		incidenciaobug:8,
-		soporte:2,
-		mantenimiento:0,
-		epic:5,
-		l:8,
-		m:2,
-		s:0,
-		xl:8,
-		xs:2,
-		xxl:0,
-		xxs:0,
-		back:8,
-		calculo:2,
-		front:0
-	},
-	{
-		list: "Next sprint",
-		nourgente: 2,
-		fueradesprint: 8,
-		feature: 6,
-		incidenciadeseguridadolegal:1,
-		incidenciaobug:4,
-		soporte:5,
-		mantenimiento:0,
-		epic:9,
-		l:2,
-		m:3,
-		s:1,
-		xl:4,
-		xs:2,
-		xxl:1,
-		xxs:5,
-		back:7,
-		calculo:0,
-		front:0
-	},
-	{
-		list: "Sprint",
-		nourgente: 5,
-		fueradesprint: 4,
-		feature: 5,
-		incidenciadeseguridadolegal:1,
-		incidenciaobug:6,
-		soporte:5,
-		mantenimiento:0,
-		epic:6,
-		l:3,
-		m:1,
-		s:4,
-		xl:7,
-		xs:5,
-		xxl:0,
-		xxs:1,
-		back:2,
-		calculo:2,
-		front:0
-	},
-	{
-		list: "To Do",
-		nourgente: 4,
-		fueradesprint: 3,
-		feature: 5,
-		incidenciadeseguridadolegal:1,
-		incidenciaobug:5,
-		soporte:4,
-		mantenimiento:0,
-		epic:3,
-		l:5,
-		m:1,
-		s:0,
-		xl:8,
-		xs:2,
-		xxl:0,
-		xxs:0,
-		back:4,
-		calculo:4,
-		front:0
-	},
-	{
-		list: "Doint",
-		nourgente: 5,
-		fueradesprint: 4,
-		feature: 4,
-		incidenciadeseguridadolegal:3,
-		incidenciaobug:5,
-		soporte:1,
-		mantenimiento:0,
-		epic:7,
-		l:5,
-		m:2,
-		s:0,
-		xl:4,
-		xs:2,
-		xxl:0,
-		xxs:0,
-		back:4,
-		calculo:2,
-		front:0
-	},
-	{
-		list: "Block or Testing",
-		nourgente: 5,
-		fueradesprint: 5,
-		feature: 5,
-		incidenciadeseguridadolegal:3,
-		incidenciaobug:4,
-		soporte:2,
-		mantenimiento:0,
-		epic:5,
-		l:4,
-		m:2,
-		s:0,
-		xl:2,
-		xs:2,
-		xxl:1,
-		xxs:0,
-		back:1,
-		calculo:2,
-		front:0
-	},
-	{
-		list: "Done",
-		nourgente: 5,
-		fueradesprint: 4,
-		feature: 3,
-		incidenciadeseguridadolegal:8,
-		incidenciaobug:6,
-		soporte:2,
-		mantenimiento:0,
-		epic:3,
-		l:4,
-		m:2,
-		s:0,
-		xl:8,
-		xs:2,
-		xxl:1,
-		xxs:0,
-		back:8,
-		calculo:2,
-		front:0
-	},
-	{
-		list: "Dismiss",
-		nourgente: 5,
-		fueradesprint: 4,
-		feature: 2,
-		incidenciadeseguridadolegal:6,
-		incidenciaobug:4,
-		soporte:2,
-		mantenimiento:0,
-		epic:1,
-		l:4,
-		m:2,
-		s:0,
-		xl:6,
-		xs:2,
-		xxl:0,
-		xxs:0,
-		back:8,
-		calculo:2,
-		front:0
-	}
-];
+
 const dataFetch = 'https://api.trello.com/1/boards/mxCDI6M3/cards';
 const memberFetch = 'https://api.trello.com/1/boards/mxCDI6M3/members';
 const listFetch = 'https://api.trello.com/1/boards/mxCDI6M3/lists';
 
-const dataUserCard = [
 
 class BarChart extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-		  dataTrello: []
+			dataTrello: [],
+			dataUsers: ""
 		}
 		this.lists = [];
 		this.users = [];
@@ -243,7 +74,8 @@ class BarChart extends React.Component {
 			delete u.listas
 		  })
 	
-		  users = _.values(users)
+			users = _.values(users)
+			this.setState({dataUsers: users })
 		  console.log(users)
 		})
 	  }
@@ -252,7 +84,7 @@ class BarChart extends React.Component {
 		return (
 		  <div className="App">
 			  <Chart
-		dataSource={dataUserCard}
+		dataSource={this.users}
 		title={" "}
 	  >
 		<CommonSeriesSettings
@@ -260,16 +92,17 @@ class BarChart extends React.Component {
 		  type={"bar"}
 		  hoverMode={"allArgumentPoints"}
 		/>
-	
-		<Series valueField={"backlog"} name={"Backlog"} />
-		<Series valueField={"nexsprint"} name={"Nex Sprint"} />
-		<Series valueField={"sprint"} name={"Sprint"} />
-		<Series valueField={"todo"} name={"To do"} />
-		<Series valueField={"doing"} name={"Doing"} />
-		<Series valueField={"blockedortesting"} name={"Blocked or testing"} />
-		<Series valueField={"done"} name={"Done"} />
-		<Series valueField={"Dismiss"} name={"Dismiss"} />
-	
+
+		{
+			this.users.map( () => {
+				return _.forEach(element => {
+					if(element === "user") {
+					return (<Series valueField={element} name={element} />)
+					}
+				})
+			})
+		}
+		
 	  </Chart>
 
 				</div>
