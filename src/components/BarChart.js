86eaf6cdc1984b474c7088720f59bbb9f9cdecdd
Lau <lau.mc84@gmail.com>
2018-09-18 08:16:37 +0200

    upgrade code

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -17,90 +17,99 @@ class BarChart extends React.Component {
 		super(props)
 		this.state = {
 			dataTrello: [],
-			dataUsers: "",
-			dataList: ""
+			dataUsers: [],
+			dataList: []
 		}
 		this.lists = [];
 		this.users = [];
 		this.cards = [];
-	
+
 		this.getData();
-	  }
-	
-	  getData () {
-		let self = this
-		fetch(dataFetch)
-		.then( (resp) => {
-		  return resp.json()
-		}).then (data => {
-		  self.cards = data
-		  return fetch(memberFetch)
-		}).then (resp => {
-		  return resp.json()
-		}).then(data => {
-		  self.members = data
-		  return fetch(listFetch)
-		}).then(resp => {
-		  return resp.json()
-		}).then(data => {
-			self.lists = data
-			this.setState({dataLists: data})
-
-		  let users = _.map(self.members, (member) => {
-			let v = {
-			  idMember: member.id,
-			  user: member.fullName,
-			  listas: []
-			}
-	
-			_.forEach(self.lists, l => {
-			  v.listas[l.id] = 0
-			})
-	
-			return v
-		  })
-	
-		  users = _.keyBy(users, 'idMember')
-	
-		  _.forEach(self.cards, card => {
-			_.forEach(card.idMembers, idMember => {
-			  users[idMember].listas[card.idList]++
-			})
-		  })
-	
-		  _.forEach(users, u => {
-			_.forEach(self.lists, l => {
-			  u[l.name.toLowerCase()] = u.listas[l.id]
-			})
-			delete u.idMember
-			delete u.listas
-		  })
-	
-			users = _.values(users)
-			this.setState({dataUsers: users })
-		})
-	  }
-	
-	  render() {
-		return (
-		  <div className="App">
-			  <Chart
-		dataSource={this.state.dataUsers}
-		title={" "}
-	  >
-		<CommonSeriesSettings
-		  argumentField={"user"}
-		  type={"bar"}
-		  hoverMode={"allArgumentPoints"}
-		/>
-			
-			<Series valueField={"backlog"} name={"Backlog"} />
-			
-	  </Chart>
-
-				</div>
-			);
-		}
 	}
 
-	export default BarChart;
+	getData() {
+		let self = this
+		fetch(dataFetch)
+			.then((resp) => {
+				return resp.json()
+			}).then(data => {
+				self.cards = data
+				return fetch(memberFetch)
+			}).then(resp => {
+				return resp.json()
+			}).then(data => {
+				self.members = data
+				return fetch(listFetch)
+			}).then(resp => {
+				return resp.json()
+			}).then(data => {
+				self.lists = data
+
+				let users = _.map(self.members, (member) => {
+					let v = {
+						idMember: member.id,
+						user: member.fullName,
+						listas: []
+					}
+
+					_.forEach(self.lists, l => {
+						v.listas[l.id] = 0
+					})
+
+					return v
+				})
+
+				users = _.keyBy(users, 'idMember')
+
+				_.forEach(self.cards, card => {
+					_.forEach(card.idMembers, idMember => {
+						users[idMember].listas[card.idList]++
+					})
+				})
+
+				_.forEach(users, u => {
+					_.forEach(self.lists, l => {
+						u[l.name.toLowerCase()] = u.listas[l.id]
+					})
+					delete u.idMember
+					delete u.listas
+				})
+
+				users = _.values(users)
+
+				this.setState({ dataUsers: users, dataList: data })
+
+			})
+	}
+
+	render() {
+		return (
+			<div className="App">
+
+				<Chart
+					dataSource={this.state.dataUsers}
+					title={" "}
+				>
+					<CommonSeriesSettings
+						argumentField={"user"}
+						type={"bar"}
+						hoverMode={"allArgumentPoints"}
+					/>
+
+					{
+						_.map(this.state.dataList, item => {
+							return (
+								<Series key={item.id} valueField={item.name.toLowerCase()} name={item.name} />
+							)
+						})
+					}
+
+					<Series valueField={item.name.toLowerCase()} name={item.name} />
+					<Series valueField={item.name.toLowerCase()} name={item.name} />
+
+				</Chart>
+			</div>
+		)}
+}
+
+export default BarChart;
