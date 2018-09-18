c4a6299db3d53df0baa2d7d8274fc3e257d991ca
Anna <annya.branco@gmail.com>
2018-09-18 12:14:02 +0200

    Chart List Labels generated with real data

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import Chart, {
 	CommonSeriesSettings,
-	Series
+	Series,
+	Legend,
+	Tooltip
 } from "devextreme-react/ui/chart";
 import * as _ from 'lodash';
+import Keys from '../keys/keys';
 
 
+const dataFetch = `https://api.trello.com/1/boards/BqZWFU8v/cards?key=${Keys.trello.key}&token=${Keys.trello.token}`;
+const memberFetch = `https://api.trello.com/1/boards/BqZWFU8v/members?key=${Keys.trello.key}&token=${Keys.trello.token}`;
+const listFetch = `https://api.trello.com/1/boards/BqZWFU8v/lists?key=${Keys.trello.key}&token=${Keys.trello.token}`;
 
-const dataFetch = 'https://api.trello.com/1/boards/mxCDI6M3/cards';
-const memberFetch = 'https://api.trello.com/1/boards/mxCDI6M3/members';
-const listFetch = 'https://api.trello.com/1/boards/mxCDI6M3/lists';
-
+let dataList = [];
+let teste = [];
 
 class BarChart extends React.Component {
 	constructor(props) {
 		super(props)
+
 		this.state = {
 			dataTrello: [],
-			dataUsers: [],
+			dataUsers: null,
 			dataList: []
 		}
+
 		this.lists = [];
 		this.users = [];
 		this.cards = [];
@@ -82,32 +88,79 @@ class BarChart extends React.Component {
 			})
 	}
 
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.dataUsers !== this.state.dataUsers) {
+			this.getLists();
+		}
+	}
+
+	getLists() {
+
+		console.log(this.state.dataUsers);
+
+
+		  for (const object of this.state.dataUsers) {
+
+		    for (let i in object) {
+
+		     if (typeof object[i] === 'number') {
+		        dataList.push(i)
+		     }
+		   }
+		  }
+		  dataList = [ ...new Set(dataList) ];
+		  console.log(dataList);
+
+		  for (const list of dataList) {
+			  teste.push(`<Series valueField="${list}" name="${list}" />`);
+		  }
+		  console.log(teste);
+	}
+
 	render() {
 		return (
 			<div className="App">
 
-				<Chart
-					dataSource={this.state.dataUsers}
-					title={" "}
-				>
-					<CommonSeriesSettings
-						argumentField={"user"}
-						type={"bar"}
-						hoverMode={"allArgumentPoints"}
-					/>
+				{this.state.dataUsers ?
 
-					{
-						_.map(this.state.dataList, item => {
-							return (
-								<Series key={item.id} valueField={item.name.toLowerCase()} name={item.name} />
-							)
-						})
-					}
+					<Chart
+						dataSource={this.state.dataUsers}
+						title={" "}
+					>
+						<CommonSeriesSettings
+							argumentField={"user"}
+							type={"bar"}
+							hoverMode={"allArgumentPoints"}
+						/>
 
-					<Series valueField={item.name.toLowerCase()} name={item.name} />
-					<Series valueField={item.name.toLowerCase()} name={item.name} />
+						{/* { dataList.map( list => <Series valueField={list} name={list} />) } */}
 
-				</Chart>
+						{/* {teste.map( item => item )} */}
+
+					<Series valueField="documentación" name="documentación" />
+					<Series valueField="roles" name="roles" />
+					<Series valueField="hitos" name="hitos" />
+					<Series valueField="backlog" name="backlog" />
+					<Series valueField="sprint backlog" name="sprint backlog" />
+					<Series valueField="en progreso" name="en progreso" />
+					<Series valueField="en aprobación/validación/pr" name="en aprobación/validación/pr" />
+					<Series valueField="terminadas" name="terminadas" />
+					<Series valueField="bloqueadas" name="bloqueadas" />
+					<Series valueField="sprints" name="sprints" />
+
+						<Legend
+							verticalAlignment={"top"}
+							horizontalAlignment={"right"}
+						/>
+
+						<Tooltip
+		                    enabled={true}
+		                    shared={true}
+						/>
+
+					</Chart>
+				: null
+				}
 			</div>
 		)}
 }
