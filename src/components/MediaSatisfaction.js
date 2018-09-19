fc64e47c473c2ec80fe1dcb7ffc3016d699ba700
Anna <annya.branco@gmail.com>
2018-09-19 20:04:04 +0200

    fix: media

--- src/components/MediaSatisfaction.js
+++ src/components/MediaSatisfaction.js
@@ -7,24 +7,6 @@ let jobMedia = [];
 
 class MediaSatisfaction extends React.Component {
 
-  componentDidMount() {
-    this.calculateMedias();
-  }
-
-  calculateMedias = () => {
-    const dataUserSatisfaction = this.props.dataUserSatisfaction;
-
-    for (const user of dataUserSatisfaction) {
-      initialMedia.push(user.initialstate);
-      finalMedia.push(user.finalestate);
-      jobMedia.push(user.dailysatisfaction);
-    }
-
-    initialMedia = Math.round((initialMedia.reduce((a, b) => a + b) / initialMedia.length) * 10) / 10
-    finalMedia = Math.round((finalMedia.reduce((a, b) => a + b) / finalMedia.length) * 10) / 10
-    jobMedia = Math.round((jobMedia.reduce((a, b) => a + b) / jobMedia.length) * 10) / 10
-  }
-
   render() {
     return (
       <div className="satisfation__media-equip">
@@ -33,15 +15,15 @@ class MediaSatisfaction extends React.Component {
         </div>
         <div className="satisfaction__media-equip--data media-equip__initial-data">
           Initial media
-          <span className={`media_numbers`}>{initialMedia}</span>
+          <span className="media_numbers">5</span>
         </div>
         <div className="satisfaction__media-equip--data media-equip__end-data">
           Final media
-          <span className="media_numbers">{finalMedia}</span>
+          <span className="media_numbers">6</span>
         </div>
         <div className="satisfaction__media-equip--data media-equip__job-data">
           Job media
-          <span className="media_numbers">{jobMedia}</span>
+          <span className="media_numbers">8</span>
         </div>
       </div>
     );
